855d5876989258352c7ae1ddad679bfe9ca11367
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-01 16:22:19 -0400

    Update top.php

--- top.php
+++ top.php
@@ -14,7 +14,7 @@ $pathParts = pathinfo($phpSelf);
     <link href = "css/layout-phone.css?version=<?php print time(); ?>" media = "(max-width: 430px)" rel = "stylesheet" type = "text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Oliver Uchitelle">
-    <meta name="description" content="Homepage of my website describing food in the United States and the why the standard american diet is so unhealthy">
+    <meta name="description" content="Website exploring NBA playoff matchups and the MVP race">
     <!--We want to discuss the NBA playoffs, reegular season awards, and what we think will happen. -->
 </head>
 
