4c296c2e92fb6f5152a646d7c246c6fabf046ccf
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-01 16:20:10 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
 <footer>
-    <p><a href="../sitemap.php">Sitemap</a></p>
+    <p><a href="../sitemap.php">Oliver Uchitelle</a></p>
+    <p><a href="https://grothenb.w3.uvm.edu/cs1080/sitemap.php">Gabe Rothenberg</a></p>
 </footer>
