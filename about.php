c16b9edc9dbe4ceb9f7dbf9e487b54b921fa974f
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-01 16:19:27 -0400

    Update about.php

--- about.php
+++ about.php
@@ -4,17 +4,18 @@
 <main>
     <h1>NBA Playoff Blog</h1>  
     <section>
-        <h2>Gabe</h2>
+        <h2>Article 1</h2>
+        <h3>Gabe</h2>
             <p>Content
                     
             </p>
                 
 
     </section>
-        <h2>Oliver</h2>
-        <p>
-            Content
-        </p>
+        <h2>Article 2</h2>
+        <h3>Knicks vs. 76ers</h3>
+            <p> Currently the Knicks lead the 76ers 3-2. The Knicks lost in brutal fashion in game 5, letting a lead slip in the last minute of regulation, resulting in an eventual overtime loss. Jalen Brunson who began the series slow followed up with a historic 47 point game. When Brunson was struggling, his former college teammates Josh Hart and Donte DiVincenzo stepped up. The series is headed back to Philadelphia for game 6 with the momentum very much in the 76ers favor, however, the Knicks are the better team. If both teams were to play their best, the Knicks are the favorite. Playing to the teams best is not often a reality however, especially as the away team. The knicks hope to steal game 6 on the road but if they lose, they'll have home court advantage in game 7 at Madison Square Garden. 
+            </p>
     </section>
 
 
