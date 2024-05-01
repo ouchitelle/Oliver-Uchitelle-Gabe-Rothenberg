fcfbbae9d2e4280e8b333db430e2c929bff9c369
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-01 14:04:22 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -2,74 +2,42 @@
 
 <body>  
 <main>
-    <h1>Recipes</h1>  
-    <section>
-        <h2>Breakfast</h2>
-        <h3>Cornmeal Waffles</h3>
-            <p>Ingredients: 
-                <ul>
-                    <li>1 cup whole wheat flour or all-purpose flour, or a combination of both</li>
-                    <li>1/2 cup cornmeal (use blue cornmeal for a striking presentation)</li>
-                    <li>1/2 teaspoon baking soda</li>
-                    <li>1/8 teaspoon salt</li>
-                    <li>2 tablespoons granulated sugar</li>
-                    <li>2 tablespoons oil</li>
-                    <li>1/4 cup applesauce</li>
-                    <li>1 & 1/4 cup almond milk</li>
-                    <li>2 tablespoons diceed roasted green chilies(optional)</li>
-                </ul>
+    <h1>MVP Race</h1> 
+    <table>
+                    <caption>Information About Blue Zone Regions</caption>
+                    <tr class="odd">
+                        <th>Region</th>
+                        <th>Country</th>
+                        <th>Information</th>
+                    </tr>
+                    <?php
+                    $sql = 'SELECT fldRegion, fldCountry, fldInformation FROM tblRegionInfo';
+                    $statement = $pdo->prepare($sql);
+                    $statement->execute();
+
+                    $records = $statement->fetchAll();
+
+                    foreach($records as $record){
+                        print '<tr>';
+                        print '<td>' . $record['fldRegion'] . '</td>';
+                        print '<td>' . $record['fldCountry'] . '</td>';
+                        print '<td>' . $record['fldInformation'] . '</td>';
+                        print '<tr>' . PHP_EOL;
+                    }
+                    ?>
+                    <tr>
+                        <td colspan="3">Source: <cite><a href="https://www.healthline.com/nutrition/blue-zones#TOC_TITLE_HDR_3"><em>healthline</em></a>
+                </tr>
+                </table>
+
+</section>
+<h2>Jokic for MVP</h2>
+            <p>
             Instructions: In a large bowl, mix the dry ingredients (the first five).
 Then add in the wet ingredients and chilies (if using, for a U.S. southwestern style) until thoroughly combined. Heat a waffle iron and spray with oil. Cook the waffles for 3 to 4 minutes. Serve immediately with toppings of your choice.
-                    
             </p>
                 
 
-    </section>
-        <h2>Lunch</h2>
-        <h3>Winter Potatoe Salad</h3>
-        <p>Ingredients: 
-                <ul>
-                    <li>8 cups water</li>
-                    <li>2 pounds patatoes, peeled and cut into quarters or eighths</li>
-                    <li>1/2 cup chopped fresh dill</li>
-                    <li>1/2 cup extra-virgin olive oil</li>
-                    <li>3 to 5 tablespoons red wine vinegar</li>
-                    <li>Salt and pepper to taste</li>
-                    <li>2 cups arugula, chopped</li>
-                    <li>2 cups spinach, chopped</li>
-                    <li>1 large sweet onion(like Vidalia), thinly sliced</li>
-                    <li>1 small head green leaf or romaine lettuce, chopped</li>
-                    <li>1 small radish, sliced(optional, for garnihs)</li>
-                </ul>
-            Instructions: Bring 8 cups of water in saucepan to boil. Add potatoes and cook uncovered until tender, about 12 minutes. Test by piercing potatoes with a fork—if they pierce easily, they are ready. Drain potatoes and let cool. In a small bowl, combine dill, olive oil, and vinegar; season with salt and pepper to taste. Whisk until well combined. In a large serving bowl, combine potatoes with dressing and toss well. Just before serving, add remaining ingredients through lettuce and toss to combine. Garnish with radish, if using.
-        </p>
-    </section>
-    <section>
-    <h2>Dinner</h2>
-    <h3>Lebanese-Style Lentil Soup</h3>
-        <p>Ingredients: 
-                <ul>
-                    <li>3 cups red lentils</li>
-                    <li>12 cups water</li>
-                    <li>1 large red onion, diced</li>
-                    <li>2 small Yukon gold potatoes, diced</li>
-                    <li>1 medium zucchini, diced</li>
-                    <li>4 cups Swiss chard, chopped</li>
-                    <li>1 bunch of cilantro, chopped</li>
-                    <li>1/4 cup of lemon juice, more to taste</li>
-                    <li>1 teaspoon cumin</li>
-                    <li>Salt and pepper, to taste</li>
-                </ul>
-                In a large pot, add lentils and water and place on medium heat to boil. In a pan, sauté onions until pink, about 5 minutes. Add chopped Swiss chard and sauté together until chard is wilted, 2-3 minutes. After lentils have cooked about 8 minutes, add onion chard mixture, along with diced potatoes and zucchini to the boiling water. Add salt and pepper to taste. Reduce heat to medium-low, cover the pot, and cook until the potatoes and lentils are completely cooked, about 12 minutes. Add the chopped cilantro. Stir and cook for about 5 minutes. Add lemon juice and cumin before serving.
-                <a href="https://www.bluezones.com/recipes/">BlueZones</a>
-                
-                   
-                <figure>
-                    <img class="images" alt="Lentil Soup" src="../lab3/images/lentils.jpg">
-                    <figcaption><a href="https://www.bluezones.com/recipes/"></a>Lentil Soup</figcaption>
-            </figure>
-        </p>
-    </section>
 
     <?php include "footer.php";?>
 
