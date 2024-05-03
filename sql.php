8609a437df57210ed63512d5ae2b635e646a3f18
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-03 12:41:00 -0400

    Update sql.php

--- sql.php
+++ sql.php
@@ -7,21 +7,21 @@ include "top.php";
 <pre>
     CREATE TABLE tblRegionInfo(
         pmkRegionInfoId INT AUTO_INCREMENT PRIMARY KEY,
-        fldRegion VARCHAR(40),
-        fldCountry VARCHAR(200),
-        fldInformation VARCHAR(200)
+        fldPlayer VARCHAR(40),
+        fldPRA VARCHAR(200),
+        fldOdds VARCHAR(200)
     )
 
     INSERT INTO tblRegionInfo
-    (fldRegion, fldCountry, fldInformation)
+    (fldPlayer, fldPRA, fldOdds)
     VALUES
-    ("Icaria", "Greece", "Residents eat a Mediterranean diet rich in olive oil, red wine, and homegrown vegetables."),
-    ("Okinawa", "Japan", "Home to some of the oldest women, residents eat a lot of soy, and practice tai chi, a slow and intentional Chinese martial art."),
-    ("Ogliastra, Italy", "Costa Rica", "Home to some of the oldest men in the world, residents live in mountainous regions, typically work on farms, and drink a lot of red wine"),
-    ("Nicoya", "Costa Rica", "Eat a lot of beans and corn torillas. Residents often participate in physically demandings jobs into old age, and have a sense of purpose called 'plan de vida'"),
-    ("Loma Linda", "United States of America", "Residents belong to a very religious group called The Seventh-day Adventists, are vegetarian, and live in tight-knit communities.")
+    ("Nikola Jokic", "47.8", "-6600"),
+    ("Luka Doncic", "52.9", "+1600"),
+    ("Shai Gilgeous-Alexander", "41.8", "+3000"),
+    ("Jalen Brunson", "39.0", "+12500"),
+    ("Giannis Antetokounmpo", "48.4", "+25000")
 
-    SELECT fldRegion, fldCountry, fldInformation FROM tblRegionInfo;
+    SELECT fldPlayer, fldPRA, fldOdds FROM tblRegionInfo;
 
 </pre>
 
