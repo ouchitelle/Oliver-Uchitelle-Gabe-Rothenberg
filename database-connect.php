14c5019c75658b13a29b7218681638cb424c2bb4
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-05 21:38:47 -0400

    Update database-connect.php

--- database-connect.php
+++ database-connect.php
@@ -2,8 +2,8 @@
 $databaseName = 'OUCHITEL_labs';
 $dsn = "mysql:host=webdb.uvm.edu;dbname=" . $databaseName;
 $username = 'ouchitel_writer';
-$password = 'nq{xt_,>uq+QUEyn%xy!';
+$password = 'oMicivdV:9})).F!v4u,';
 
-    $pdo = new PDO($dsn, $username, $password);
-    if($pdo) print '<!-- Connection complete -->';
-    ?>
+$pdo = new PDO($dsn, $username, $password);
+if($pdo) print '<!-- Connection complete -->';
+?>
