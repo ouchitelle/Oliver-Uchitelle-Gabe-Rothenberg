2887d217f4455f2623abe423867cc87f6ba013ca
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2024-05-05 21:39:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include "top.php";
                     <figcaption><a href="https://www.cbssports.com/nba/news/2024-nba-playoffs-bracket-schedule-games-today-scores-76ers-vs-knicks-lakers-vs-nuggets-in-game-3/"></a>Playoff Bracket</figcaption>
             </figure>
             <ul>
-                    <li><em>New York Knicks vs. Philadelphia 76ers</em>em></li>
+                    <li>New York Knicks vs. Philadelphia 76ers</li>
                     <li>Clevland Cavaliers vs. Orlando Magic</li>
                     <li>Milkwalkee Bucks vs. Indiana Pacers</li>
                     <li>Boston Celtics vs. Miami Heat</li>
@@ -21,17 +21,13 @@ include "top.php";
                     <li>Oklahoma City Thunder vs. New Orleans Pelicans</li>
                     <li>Milkwalkee Bucks vs. Indiana Pacers</li>
                 </ul>
-        <h2>Round 1 Spotlight: NYK v. PHI</h2>
-            <p>The New York Knicks vs. The Philadelphia 76ers is our spotlighted matchup for Round 1.  
-                With the 2023 MVP Joel Embiid returning from injury for the 76ers and key pieces like Mitchell Robinson and OG Anunoby returning to the rotation for New York, 
-                both teams are looking to take the series with their revamped rosters. New York and Philadelphia have two of the most dedicated fanbases in the league, so it should come as no surprise that this will be an intense series.
-                As Embiid returns to his old self, Tyrese Maxey has taken the Sixers by the reins. He received this year's Most Improved Player Award. On the other side of the battlefield is Jalen Brunson,
-                New York's superstar. The lefty is a crafty finisher around the rim, and backs his inside scoring with an impressive 40% shooting percentage from beyond the 3 point arc.
-                This series will be a tight battle, making it our spotlight for the week. 
-            </p>
+        <h2>Round 1 Spotlight</h2>
+        <h3>Nuggets vs. Lakers</h3>
+            <p>The Los Angeles Lakers recently lost to the Denver Nuggets. The Nuggets are coming off a champtionship and are looking to repeat. The Lakers won in 2020, but have not managed to have similar success in recent year. The Lakers are led by superstars Lebron James and Anthony Davis, both who have had amazing careers in the NBA, but the Nuggets dynamic duo of Nikola Jokic (soon to be 3 time MVP), and Jamal Murray (one of the most clutch playoff performers in NBA history) was too much for the aging Lakers stars and weak roll players. </p>
             
-            
-        
+            <h3>Timberwolves vs. Suns</h3>
+            <p>The Phoenix Suns were overpowered by the Timberwolves in the first round of the playoffs. The Suns core of Kevin Durant, Devin Booker, and Bradly Beal was not enough to get past the height of Karl Anthony Towns and Rudy Gobert, and the young superstar Anthony Edwards. The Timberwolves are looking to make a name for themselves as contenders, and beating a Suns team that was in the finals only a couple years ago is a step in the right direction. Next, they face the former champions, and the MVP. Will KAT and Gobert be enough to stop Jokic?
+</p>
 
     
     <?php include "footer.php";?>
