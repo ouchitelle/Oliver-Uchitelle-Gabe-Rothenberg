21beb10daa2212fe62e6e79484eb734b1e298d4b
GabeRothenberg <157160903+GabeRothenberg@users.noreply.github.com>
2024-05-08 19:26:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,14 @@ include "top.php";
                     <li>Milkwalkee Bucks vs. Indiana Pacers</li>
                 </ul>
         <h2>Round 1 Spotlight</h2>
+        <h3>Round 1 Spotlight: NYK v. PHI</h3>
+            <p>The New York Knicks vs. The Philadelphia 76ers is our spotlighted matchup for Round 1.  
+                With the 2023 MVP Joel Embiid returning from injury for the 76ers and key pieces like Mitchell Robinson and OG Anunoby returning to the rotation for New York, 
+                both teams are looking to take the series with their revamped rosters. New York and Philadelphia have two of the most dedicated fanbases in the league, so it should come as no surprise that this will be an intense series.
+                As Embiid returns to his old self, Tyrese Maxey has taken the Sixers by the reins. He received this year's Most Improved Player Award. On the other side of the battlefield is Jalen Brunson,
+                New York's superstar. The lefty is a crafty finisher around the rim, and backs his inside scoring with an impressive 40% shooting percentage from beyond the 3 point arc.
+                This series will be a tight battle, making it our spotlight for the week. 
+            </p>
         <h3>Nuggets vs. Lakers</h3>
             <p>The Los Angeles Lakers recently lost to the Denver Nuggets. The Nuggets are coming off a champtionship and are looking to repeat. The Lakers won in 2020, but have not managed to have similar success in recent year. The Lakers are led by superstars Lebron James and Anthony Davis, both who have had amazing careers in the NBA, but the Nuggets dynamic duo of Nikola Jokic (soon to be 3 time MVP), and Jamal Murray (one of the most clutch playoff performers in NBA history) was too much for the aging Lakers stars and weak roll players. </p>
             
